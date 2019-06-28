2ad0822847eb76cdff65a96ea055c309d3bba8cb
Ben Oxley <ben.oxley@gmail.com>
2019-06-29 00:32:21 +0100

    Got sum tile 🗺

--- server/map.py
+++ server/map.py
@@ -1,9 +1,10 @@
 import pygame
+from tile import Tile
 OPEN_TILE = 0
 HIGH_NOISE = 1
 SOURCE_TILE = 2
 SINK_TILE = 3
-BLACK = (0,0,0)
+BLACK = (30,30,30)
 BROWN = (153,76,0)
 GREEN = (30,255,30)
 BLUE = (30,40,255)
@@ -14,21 +15,26 @@ COLOURS = {
         SOURCE_TILE : GREEN,
         SINK_TILE : BLUE
     }
-TILESIZE = 40
+TILESIZE = 200
 
 class Map:
     tilesize = TILESIZE
+    grass_img = pygame.image.load('resources\grass-200.png')
+    grass_inv_img = pygame.image.load('resources\grass-inv-200.png')
     tilemap = [
-        [OPEN_TILE,OPEN_TILE,OPEN_TILE],
-        [HIGH_NOISE,SOURCE_TILE,OPEN_TILE],
-        [OPEN_TILE,OPEN_TILE,HIGH_NOISE],
-        [SOURCE_TILE,OPEN_TILE,OPEN_TILE]
+        [grass_img,grass_img,grass_img],
+        [grass_img,grass_inv_img,grass_img],
+        [grass_inv_img,grass_img,grass_img],
+        [grass_img,grass_img,grass_inv_img]
         ]
     height = 4
     width = 3
-
+    
     def show(self,screen):
+        
         for row in range(self.height): 
             for col in range(self.width):
-                pygame.draw.rect(screen,COLOURS[self.tilemap[row][col]],(col*self.tilesize,row*self.tilesize,self.tilesize,self.tilesize))
-    
+                screen.blit(self.tilemap[row][col], (col*self.tilesize,row*self.tilesize,self.tilesize,self.tilesize))
+                #Tile((col*self.tilesize,row*self.tilesize,self.tilesize,self.tilesize))
+                #pygame.draw.rect(screen,COLOURS[self.tilemap[row][col]],(col*self.tilesize,row*self.tilesize,self.tilesize,self.tilesize))
+                
---
+++ server/tile.py
@@ -0,0 +1,16 @@
+
+import pygame
+
+
+GRASS_TILE = None
+
+
+class Tile(pygame.sprite.Sprite):
+    
+    GRASS_TILE = None
+    def __init__(self, pos):
+        if self.GRASS_TILE is None:
+            self.GRASS_TILE = pygame.image.load('resources\grass-200.png').convert_alpha()
+        super().__init__()
+        self.image = self.GRASS_TILE
+ 
