9ec5c1366bb418daae05a4ab45d0419859a5c47e
Tom Dalby <thomasjackdalby@gmail.com>
2019-06-28 23:40:13 +0100

    fixed

--- server/node.py
+++ server/node.py
@@ -17,6 +17,6 @@ class Node:
         pass
 
     def show(self, screen):
-        if self.is_on:
+        if self.is_placed:
             pygame.draw.ellipse(screen, DARK_RED, [self.pos.x-NODE_SIZE/2, self.pos.y-NODE_SIZE/2, NODE_SIZE, NODE_SIZE], 2)
             pygame.draw.ellipse(screen, RED, [self.pos.x-NODE_RADIUS/2, self.pos.y-NODE_RADIUS/2, NODE_RADIUS, NODE_RADIUS], 2)
