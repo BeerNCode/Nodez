9804ab1637fda946b0bf03916790ef501d31562d
Ben Oxley <ben.oxley@gmail.com>
2019-06-28 22:59:00 +0100

    Adding map

--- server/program.py
+++ server/program.py
@@ -5,6 +5,7 @@ import threading
 import logging
 import colours
 from player import Player
+from map import Map
 
 from tools import *
 
@@ -15,8 +16,28 @@ SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 768
 GAME_SPEED = 30
 
+OPEN_TILE = 0
+HIGH_NOISE = 1
+SOURCE_TILE = 2
+SINK_TILE = 3
+BLACK = (0,0,0)
+BROWN = (153,76,0)
+GREEN = (30,255,30)
+BLUE = (30,40,255)
+RED = (255,40,55)
+COLOURS = {
+        OPEN_TILE : BLACK,
+        HIGH_NOISE : RED,
+        SOURCE_TILE : GREEN,
+        SINK_TILE : BLUE
+    }
+TILESIZE = 40
+
+
 IP_ADDRESS = "0.0.0.0"
 PORT = 5000
+
+
 
 logger = logging.getLogger(__name__)
 
@@ -55,6 +76,10 @@ class Program:
 
         pygame.draw.ellipse(self.screen, colours.WHITE, [SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 100, 100], 2)
 
+        map = Map()
+        for row in range(map.height): 
+            for col in range(map.width):
+                pygame.draw.rect(self.screen,COLOURS[map.tilemap[row][col]],(map.height*map.tilesize,map.width*map.tilesize,map.tilesize,map.tilesize))
         sprites = pygame.sprite.Group()
         for idx, player in enumerate(self.players):
             player.show(self.screen)
