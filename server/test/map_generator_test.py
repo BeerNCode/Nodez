cd9e0acd71e1bcd098ca40ce288825d30a04d7c4
lb0608 <lb0608@my.bristol.ac.uk>
2019-06-29 16:58:02 +0100

    test update

--- server/test/map_generator_test.py
+++ server/test/map_generator_test.py
@@ -6,8 +6,16 @@ def test_generate_block_map():
 
     tileTypeArray = mapGen.generate_block_map(0)
 
-    print(tileTypeArray)
+    assert len(tileTypeArray) == 16
+    assert len(tileTypeArray[0]) == 8
+    assert all(map(lambda col: all(map(lambda val: val == 0, col)), tileTypeArray))
 
-    assert tileTypeArray.count == 16
-    assert tileTypeArray[0].count == 8
-    assert all(lambda col: all(lambda val: val == 0, col), tileTypeArray)
+def test_generate_block_map_with_holes():
+    mapGen = MapGenerator((512,256), 32)
+
+    tileTypeArray = mapGen.generate_block_map(3)
+
+    assert len(tileTypeArray) == 16
+    assert len(tileTypeArray[0]) == 8
+    assert any(map(lambda col: any(map(lambda val: val == -1, col)), tileTypeArray))
+
