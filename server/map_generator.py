940145f6c785b7e8872b4a06f0c54fcbcc42f1b1
lb0608 <lb0608@my.bristol.ac.uk>
2019-06-29 13:33:38 +0100

    Added translation of generated map into tile types.

--- server/map_generator.py
+++ server/map_generator.py
@@ -25,7 +25,7 @@ class MapGenerator:
                     output[tile[0]][tile[1]] = -1
             n-=1
 
-        return output
+        return self.transform_map(output)
 
     def getRandomTilesForFeature(self):
         x = random.randint(0, self.numX-1)
@@ -52,3 +52,38 @@ class MapGenerator:
     
     def get_direction(self):
         return random.randint(0, 4)
+
+    def transform_map(self, map_tiles):
+        output = [[0] * self.numY for i in range(self.numX)]
+
+        max_x = self.numX - 1
+        max_y = self.numY - 1
+
+        for ix in range(self.numX):
+            for iy in range(self.numY):
+                if (map_tiles[ix][iy] < 0):
+                    output[ix][iy] = 15
+                    continue
+                
+                if (iy == 0 or map_tiles[ix][iy-1] < 0):
+                    output[ix][iy] += 8
+                if (ix == max_x or map_tiles[ix+1][iy] < 0):
+                    output[ix][iy] += 4
+                if (iy == max_y or map_tiles[ix][iy+1] < 0):
+                    output[ix][iy] += 2
+                if (ix == 0 or map_tiles[ix-1][iy] < 0):
+                    output[ix][iy] += 1
+        
+        return list(map(lambda col: list(map(self.transform_value, col)), output))
+
+    def transform_value(self, val):
+        output = ''
+        if (val & 8 == 8): output += 'X'
+        else: output += 'O'
+        if (val & 4 == 4): output += 'X'
+        else: output += 'O'
+        if (val & 2 == 2): output += 'X'
+        else: output += 'O'
+        if (val & 1 == 1): output += 'X'
+        else: output += 'O'
+        return output
